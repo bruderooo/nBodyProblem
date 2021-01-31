4934fe7c35e8a7f21272c90ed58f75b5eb8478f7
bruderooo <szymonjacon@gmail.com>
2021-01-31 18:40:12 +0100

    Add one_epoch and start_simulation for World.py. Del Vector.py, because of new format of Body.py.

--- Include/Body.py
+++ Include/Body.py
@@ -1,24 +1,47 @@
 from __future__ import annotations
 
-from typing import List
+from typing import List, Union
 
-from numpy import G
 import numpy as np
-import matplotlib.pyplot as plt
-
-from Include.Vector import Vector
+from numpy import G, ndarray
+from numpy.linalg import norm
 
 
 class Body:
 
-    def __init__(self, mass: float, position: List[float], velocity: List[float]) -> None:
-        self.mass = mass
-        self.position = Vector(position)
-        self.velocity = Vector(velocity)
-        self.acceleration = Vector(np.zeros(len(velocity)))
+    def __init__(self, mass: Union[float, None],
+                 position: Union[List[float], ndarray[float]] = None,
+                 velocity: Union[List[float], ndarray[float]] = None,
+                 random_val=False, dim=2) -> None:
+
+        self.mass = mass if mass is not None else np.random.uniform(-10, 10)
+        self.acceleration = np.zeros(dim)
+
+        if random_val or (position is not None and velocity is not None):
+            self.position = np.random.uniform(-10, 10, dim) if random_val else position
+            self.velocity = np.random.uniform(-10, 10, dim) if random_val else velocity
+
+            self.new_position = self.position
+            self.new_velocity = self.velocity
 
     def distance_to(self, body: Body):
-        return self.position.euclidean_distance(body.position)
+        # np.linalg.norm(A - B)
+        # this is euclidean distance A to B
+        return np.linalg.norm(self.position - body.position)
 
     def force_to(self, body: Body):
         return (G * self.mass * body.mass) / self.distance_to(body) ** 2
+
+    def compute(self, body: Body):
+        self.new_position = self.position + (self.velocity + 0.5 * self.acceleration)
+        self.new_velocity = self.velocity + self.acceleration
+
+        distance_abs = np.absolute(self.position - body.position)
+        self.acceleration += (G * body.mass / pow(distance_abs, 3)) * distance_abs
+
+    def update(self):
+        self.position = self.new_position
+        self.velocity = self.new_velocity
+
+    def __eq__(self, other):
+        return hex(id(self)) == hex(id(other))
--- Include/World.py
+++ Include/World.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import time
 from typing import List
 import numpy as np
 
@@ -8,7 +9,26 @@ from Include.Body import Body
 
 class World:
 
-    def __init__(self, bodies: int, cycles: int, file=None) -> None:
+    def __init__(self, no_bodies: int, file=None) -> None:
         self.bodies: List[Body] = []
-        # for i in range(bodies):
-        #     self.bodies += [Body()]
+        if file is None:
+            for _ in range(no_bodies):
+                self.bodies += [Body(1, random_val=True)]
+        else:
+            for _ in range(no_bodies):
+                # self.bodies += [Body()]
+                pass
+
+    def start_simulation(self, cycles: int, duration: float = 0):
+        for _ in range(cycles):
+            self.one_epoch()
+            time.sleep(duration)
+
+    def one_epoch(self):
+        for i, body1 in enumerate(self.bodies):
+            for j, body2 in enumerate(self.bodies):
+                if i != j:
+                    body1.compute(body2)
+
+        for body in self.bodies:
+            body.update()
